88a2770e5bd3d7fb356a609dc2c70d01a298e4cd
Sean Doyle <sdoylelambda@gmail.com>
2019-03-01 12:45:02 -0500

    First half of object-arrays.js completed.

--- challenges/functions.js
+++ challenges/functions.js
@@ -28,15 +28,15 @@
 // Explanation: 
 
 
-const external = "I'm outside the function";
+// const external = "I'm outside the function";
 
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+// function myFunction() {
+//   console.log(external);
+//   const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
-}
-myFunction();
+//   function nestedFunction() {
+//     console.log(internal);
+//   };
+//   nestedFunction();
+// }
+// myFunction();
