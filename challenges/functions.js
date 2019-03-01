d0ff0c192195effb8b0032e201e5a26d4d3a4f68
Sean Doyle <sdoylelambda@gmail.com>
2019-03-01 18:53:14 -0500

    Functions completed, minor corrections needed.

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,8 +7,8 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-function comsume(param1, param2, callback) {
-  return callbackFunction(param1, param2);
+function consume(param1, param2, callBack) {
+  return callBack(param1, param2);
 }
 
 
@@ -19,11 +19,23 @@ function comsume(param1, param2, callback) {
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function greeting(firstName, lastName) {
+  return (`Hello ${firstName} ${lastName}, nice to meet you!`);
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
